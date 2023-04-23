cab434cd1fd1b30784c6c811c17782343fee10f0
Enrique Avila <senquevila@gmail.com>
2023-04-23 17:47:01 -0600

    Update InsertionSort.java
    Insertion sort

--- cap-17/src/InsertionSort.java
+++ cap-17/src/InsertionSort.java
@@ -2,39 +2,34 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class InsertionSort<T extends Comparable> {
-    public void sort(List<T> lista) {
-        for (int i = 1; i < lista.size(); i++) {
-            T valI = lista.get(i);
+public class InsertionSort {
+    static int iteraciones = 0;
+    public static <T extends Comparable<T>> void sorting(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            T currentElement = list.get(i);
             int j = i - 1;
-            while(j > 0) {
-                T valJ = lista.get(j);
-
-                if (valI.compareTo(valJ) >= 0) {
-                    break;
-                }
-
-                lista.set(j, valJ);
+            while (j >= 0 && list.get(j).compareTo(currentElement) > 0) {
+                iteraciones++;
+                list.set(j + 1, list.get(j));
                 j--;
             }
-
-            lista.set(j, valI);
+            iteraciones++;
+            list.set(j + 1, currentElement);
         }
     }
 
+
+
     public static void main(String[] args) {
-
-
         List<Integer> lista = new ArrayList<>();
-        lista.add(2);
-        lista.add(1);
+        lista.add(6);
         lista.add(4);
         lista.add(3);
-        lista.add(6);
+        lista.add(2);
+        lista.add(1);
 
-        InsertionSort<Integer> ordenador = new InsertionSort<>();
-        ordenador.sort(lista);
-
+        InsertionSort.sorting(lista);
         System.out.println(lista);
+        System.out.printf("Para un n=%d hay %d iteraciones", lista.size(), iteraciones);
     }
 }
